925dbb598f853cc2bf30e2300d78510eb85e74e4
Jptorresar <jtorresarias99@gmail.com>
2024-11-10 18:00:11 -0500

    modal fix 2

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,16 +1,15 @@
-import ReactDOM from 'react-dom';
+
 import "../styles/modal.css";
 
 const Modal = (props) => {
-  return ReactDOM.createPortal(
+  return (
     <div className="modal-overlay" onClick={props.onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <p>{props.text}</p>
         <button className="button-3" onClick={props.onClose}>Cerrar</button>
       </div>
-    </div>,
-    document.getElementById('modal-root')
-  );
+    </div>
+  ); 
 };
 
 export default Modal;
